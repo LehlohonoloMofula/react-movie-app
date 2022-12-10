675c0733e30860c6517bc21d37a15b65ce092b65
Lehlohonolo Mofula <lehlohonoloharison@gmail.com>
2022-12-10 22:59:25 +0200

    added ai and oulate the data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
+import { useEffect, useState } from 'react';
+//import 
 import './App.css';
 
 function App() {
+
+  const API_URI = "https:api.themoviedb.org/3/movie/550?api_key=94661eb5d325db1b5489a64355d8e563"
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    fetch(API_URI)
+    .then(res =>  res.json())
+    .then(data => setMovies(data))
+  },[])
+
+  console.log(movies)
   return (
     <div className="App">
-      
+      <div className="search_nav">
+        <div>
+          <h1>Movies</h1>
+
+          <div className="">
+            <form>
+              <input label="" />
+              <button>Search</button>
+            </form>
+          </div>
+        </div>
+      </div>
+
+      <div className="movies-">
+        { movies.map((movie) =>(
+          <MovieCard />
+        ))}
+      </div>
     </div>
   );
 }
